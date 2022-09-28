df1606226518787460b7caf5a8bae5c983cf3f9a
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2022-09-28 08:50:41 +0100

    comments to ifelse
    change commented package install code to conditional statements

--- Scripts/CODE_case_study.R
+++ Scripts/CODE_case_study.R
@@ -1,15 +1,22 @@
-## ----install pacakges, eval=FALSE-----------------------------------------------
-## # For Windows:
-## install.packages("survHE",
-## 	               repos=c("http://www.statistica.it/gianluca/R",
-## 		                     "https://cran.rstudio.org",
-##                          "https://inla.r-inla-download.org/R/stable"),
-## 	               dependencies=TRUE)
-## 
-## install.packages("INLA",
-##                  repos=c(getOption("repos"),
-##                          INLA="https://inla.r-inla-download.org/R/stable"),
-##                  dep=TRUE)
+
+if (.Platform$OS.type == "windows") {
+  if("survHE" %in% rownames(installed.packages()) == FALSE) {
+    install.packages("survHE",
+ 	                   repos = c("http://www.statistica.it/gianluca/R",
+ 		                         "https://cran.rstudio.org",
+                              "https://inla.r-inla-download.org/R/stable"),
+ 	                   dependencies = TRUE)
+   }
+}
+
+if (.Platform$OS.type == "windows") {
+  if("survHE" %in% rownames(installed.packages()) == FALSE) {
+    install.packages("INLA",
+                     repos = c(getOption("repos"),
+                     INLA = "https://inla.r-inla-download.org/R/stable"),
+                     dep = TRUE)
+   }
+}
 
 
 ## ----load packages, results='hide', warning=FALSE, message=FALSE----------------
